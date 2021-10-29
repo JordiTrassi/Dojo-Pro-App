3f390ac4ef18a4b8151be7ae5d13a48761aab340
JordiTrassi <j.trassi@telefonica.net>
2021-10-30 00:20:46 +0200

    Add Header in Students page

--- frontend/src/components/AppScreens/Students/StudentsHeader.jsx
+++ frontend/src/components/AppScreens/Students/StudentsHeader.jsx
@@ -2,32 +2,22 @@ import React from 'react';
 import {
   SafeAreaView,
   View,
-  Image,
   ImageBackground,
-  TouchableOpacity,
   Text,
 } from 'react-native';
 
-import PropTypes from 'prop-types';
 import styles from './StudentsHeader_Styles';
+import Header from '../Header/Header';
 import Studentscard from './StudentsCard';
 
 const imageBambo = {
   uri: 'https://theshowroommag.com/wp-content/uploads/2020/01/1239x697.jpg',
 };
-const toriIcon = {
-  uri: 'https://cdn1.iconfinder.com/data/icons/japan-line-2/48/Japan_Japanese-59-1024.png',
-};
-const logOutIcon = {
-  uri: 'https://www.seekpng.com/png/full/351-3512254_png-file-transparent-background-logout-icon.png',
-};
 
-export default function StudentsHeader({ navigation }) {
-  const onPressLogOut = () => { navigation.navigate('LogOutScreen'); };
-  const onPressProfile = () => { navigation.navigate('ProfileScreen'); };
-
+export default function StudentsHeader() {
   return (
     <SafeAreaView style={styles.container}>
+      <Header />
       <View style={styles.background_img_dashboartStack}>
         <ImageBackground
           source={imageBambo}
@@ -35,26 +25,6 @@ export default function StudentsHeader({ navigation }) {
           style={styles.background_img_dashboart}
           imageStyle={styles.background_img_dashboart_imageStyle}
         />
-        <TouchableOpacity
-          onPress={onPressLogOut}
-          style={styles.btn_logout}
-        >
-          <Image
-            source={logOutIcon}
-            resizeMode="contain"
-            style={styles.incon_logout}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={onPressProfile}
-          style={styles.btn_profile}
-        >
-          <Image
-            source={toriIcon}
-            resizeMode="contain"
-            style={styles.tori_icon}
-          />
-        </TouchableOpacity>
         <View style={styles.title_rectangle}>
           <Text style={styles.title_students}>STUDENTS</Text>
         </View>
@@ -65,9 +35,3 @@ export default function StudentsHeader({ navigation }) {
     </SafeAreaView>
   );
 }
-
-StudentsHeader.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
