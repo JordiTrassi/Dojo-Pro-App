4c6c3149ed9b2d9eac6ebcb3ff0ff9f3bce4d9e5
JordiTrassi <j.trassi@telefonica.net>
2021-11-09 15:29:00 +0100

    Refact Picker style in New Class page

--- frontend/src/components/AppScreens/TheClasses/ClassNewOne.jsx
+++ frontend/src/components/AppScreens/TheClasses/ClassNewOne.jsx
@@ -27,32 +27,6 @@ export default function ClassNewOne({ navigation }) {
   const [hour, setHour] = useState('');
   const [discipline, setDiscipline] = useState('');
 
-  // const pickerStyle = {
-  //   inputIOS: {
-  //     color: '#121212',
-  //     paddingTop: 4,
-  //     paddingHorizontal: 10,
-  //     fontSize: 25,
-  //     textAlign: 'center',
-  //   },
-  //   placeholderColor: '#121212',
-  //   underline: { borderTopWidth: 0 },
-  //   icon: {
-  //     position: 'absolute',
-  //     backgroundColor: 'transparent',
-  //     borderTopWidth: 5,
-  //     borderTopColor: '#00000099',
-  //     borderRightWidth: 5,
-  //     borderRightColor: 'transparent',
-  //     borderLeftWidth: 5,
-  //     borderLeftColor: 'transparent',
-  //     width: 0,
-  //     height: 0,
-  //     top: 20,
-  //     right: 15,
-  //   },
-  // };
-
   const validateData = () => {
     let isValid = true;
     if (discipline.length === 0 || hour.length === 0 || day.length === 0) {
@@ -92,46 +66,46 @@ export default function ClassNewOne({ navigation }) {
         </View>
         <View style={styles.form_new_class}>
           <Text style={styles.form_title_camp}>Day:</Text>
-          <View style={styles.rect_day}>
-            <Picker
-              placeholder={{ label: 'Select the class day', value: null }}
-              selectedValue={day}
-              onValueChange={(newDay) => setDay(newDay)}
-            >
-              <Picker.Item label="Monday" value="Monday" />
-              <Picker.Item label="Tuesday" value="Tuesday" />
-              <Picker.Item label="Wednesday" value="Wednesday" />
-              <Picker.Item label="Thursday" value="Thursday" />
-              <Picker.Item label="Friday" value="Friday" />
-              <Picker.Item label="Saturday" value="Saturday" />
-            </Picker>
-          </View>
+          <Picker
+            placeholder={{ label: 'Select the class day', value: null }}
+            selectedValue={day}
+            onValueChange={(newDay) => setDay(newDay)}
+            style={styles.picker_rectangle}
+            itemStyle={{ height: 60 }}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+          </Picker>
           <Text style={styles.form_title_camp}>Hour:</Text>
-          <View style={styles.rect_hour}>
-            <Picker
-              placeholder={{ label: 'Select hour', value: null }}
-              onValueChange={(newHour) => setHour(newHour)}
-            >
-              <Picker.Item label="17:00" value="17:00" />
-              <Picker.Item label="18:00" value="18:00" />
-              <Picker.Item label="19:00" value="19:00" />
-              <Picker.Item label="20:00" value="20:00" />
-              <Picker.Item label="21:00" value="21:00" />
-            </Picker>
-          </View>
-          <Text style={styles.discipline_title}>Discipline:</Text>
-          <View style={styles.rect_discipline}>
-            <Picker
-              placeholder={{ label: 'Select the discipline', value: null }}
-              selectedValue={discipline}
-              onValueChange={(newDiscipline) => setDiscipline(newDiscipline)}
-            >
-              <Picker.Item label="Karate" value="Karate" />
-              <Picker.Item label="Kata" value="Kata" />
-              <Picker.Item label="Kumite" value="Kumite" />
-              <Picker.Item label="Goshin" value="Goshin" />
-            </Picker>
-          </View>
+          <Picker
+            placeholder={{ label: 'Select hour', value: null }}
+            onValueChange={(newHour) => setHour(newHour)}
+            style={styles.picker_rectangle}
+            itemStyle={{ height: 60 }}
+          >
+            <Picker.Item label="17:00" value="17:00" />
+            <Picker.Item label="18:00" value="18:00" />
+            <Picker.Item label="19:00" value="19:00" />
+            <Picker.Item label="20:00" value="20:00" />
+            <Picker.Item label="21:00" value="21:00" />
+          </Picker>
+          <Text style={styles.form_title_camp}>Discipline:</Text>
+          <Picker
+            placeholder={{ label: 'Select the discipline', value: null }}
+            selectedValue={discipline}
+            onValueChange={(newDiscipline) => setDiscipline(newDiscipline)}
+            style={styles.picker_rectangle}
+            itemStyle={{ height: 60 }}
+          >
+            <Picker.Item label="Karate" value="Karate" />
+            <Picker.Item label="Kata" value="Kata" />
+            <Picker.Item label="Kumite" value="Kumite" />
+            <Picker.Item label="Goshin" value="Goshin" />
+          </Picker>
           <TouchableOpacity
             onPress={() => handleNewClass()}
             style={styles.button_confirm}
